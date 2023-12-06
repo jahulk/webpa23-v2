70ae9d47a85ef4d295e45de424c167c74c05c6b8
Jarkko Hulkko <jarkko.hulkko@centria.fi>
2023-12-06 12:48:42 +0200

    2.6

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,10 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
+  has_many :ratings
+
+  def average_rating
+    sum = ratings.reduce(0) { |acc, cur| acc + cur.score }
+    avg = ratings.empty? ? 0 : sum / ratings.count
+    avg.to_f
+  end
 end
