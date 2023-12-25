6e2740d87dfdecdb0683455a89fba458cfd16c9e
Jarkko Hulkko <jarkko.hulkko@centria.fi>
2023-12-25 22:09:06 +0200

    5.15

---
+++ lib/weather_api.rb
@@ -0,0 +1,24 @@
+class WeatherApi
+
+  def self.weather_in(city)
+    city = city.downcase
+    Rails.cache.fetch("weather_#{city}", expires_in: 5.minutes) { get_weather_in(city) }
+  end
+
+  def self.get_weather_in(city)
+    url = "http://api.weatherstack.com/current?access_key=#{key}&query="
+
+    response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
+    return nil if response.parsed_response["success"] == false
+
+    weather = OpenStruct.new(response.parsed_response["current"])
+    return weather
+  end
+
+  def self.key
+    return nil if Rails.env.test?
+    raise 'WEATHER_APIKEY env variable not defined' if ENV['WEATHER_APIKEY'].nil?
+
+    ENV.fetch('WEATHER_APIKEY')
+  end
+end
