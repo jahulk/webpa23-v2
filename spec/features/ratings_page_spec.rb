00490aafd968319460a97e35c70b3a2c0232ebe6
Jarkko Hulkko <jarkko.hulkko@centria.fi>
2023-12-25 13:01:29 +0200

    4.6

--- spec/features/ratings_page_spec.rb
+++ spec/features/ratings_page_spec.rb
@@ -24,5 +24,18 @@ describe "Rating" do
     expect(user.ratings.count).to eq(1)
     expect(beer1.ratings.count).to eq(1)
     expect(beer1.average_rating).to eq(15.0)
+    end
+
+  it "all ratings are rendered on ratings page" do
+    rating1 = FactoryBot.create :rating, score: 10, beer: beer1, user: user
+    rating2 = FactoryBot.create :rating, score: 7, beer: beer1, user: user
+    rating3 = FactoryBot.create :rating, score: 15, beer: beer2, user: user
+
+    visit ratings_path
+    expect(page).to have_content "Total ratings: 3"
+    expect(page).to have_content "#{rating1.beer.name} #{rating1.score} #{rating1.user.username}"
+    expect(page).to have_content "#{rating2.beer.name} #{rating2.score} #{rating2.user.username}"
+    expect(page).to have_content "#{rating3.beer.name} #{rating3.score} #{rating3.user.username}"
+
   end
 end
