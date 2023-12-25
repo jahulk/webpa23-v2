1694922362cbb28c0236fce7cb927e1d0b3eaaa4
Jarkko Hulkko <jarkko.hulkko@centria.fi>
2023-12-25 19:03:16 +0200

    5.4

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,5 +1,10 @@
 class BeermappingApi
   def self.places_in(city)
+    city = city.downcase
+    Rails.cache.fetch(city, expires_in: 7.days ) { get_places_in(city) }
+  end
+
+  def self.get_places_in(city)
     url = "http://beermapping.com/webservice/loccity/#{key}/"
 
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
@@ -11,7 +16,6 @@ class BeermappingApi
     places.map do |place|
       Place.new(place)
     end
-
   end
 
   def self.key
