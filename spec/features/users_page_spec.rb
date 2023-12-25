2c4d550e1299fd2ce1d70f28d09317aacc79271a
Jarkko Hulkko <jarkko.hulkko@centria.fi>
2023-12-25 13:29:10 +0200

    4.8 and fixed typo

--- spec/features/users_page_spec.rb
+++ spec/features/users_page_spec.rb
@@ -51,5 +51,25 @@ describe "User" do
     expect(page).to have_content("#{rating1.beer.name} #{rating1.score} Delete")
     expect(page).to have_content("#{rating2.beer.name} #{rating2.score} Delete")
 
+    end
+
+  it "can delete his own ratings" do
+    brewery = FactoryBot.create :brewery, name: "Koff"
+    beer1 = FactoryBot.create :beer, name: "iso 3", brewery:brewery
+    beer2 = FactoryBot.create :beer, name: "Karhu", brewery:brewery
+    user2 = FactoryBot.create :user, username: "Makke"
+    user3 = FactoryBot.create :user, username: "Seppo"
+    rating1 = FactoryBot.create :rating, score: 10, beer: beer1, user: user2
+    rating2 = FactoryBot.create :rating, score: 7, beer: beer1, user: user2
+    rating3 = FactoryBot.create :rating, score: 15, beer: beer2, user: user3
+
+    sign_in(username: "Makke", password: "Foobar1")
+    visit user_path(user2)
+    el = find(:xpath, "(//a[text()='Delete'])[1]")
+    expect {
+      el.click
+    }.to change{user2.ratings.count}.from(2).to(1)
+    expect(page).to_not have_content("#{rating1.beer.name} #{rating1.score} Delete")
+    expect(page).to have_content("Has made 1 rating, average rating #{rating2.score}")
   end
 end
