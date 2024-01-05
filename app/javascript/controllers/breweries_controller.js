d197c0336d1c0cc3f25b19b9e598c9d1ab0f294e
Jarkko Hulkko <jarkko.hulkko@centria.fi>
2024-01-05 17:51:32 +0200

    8.16

--- app/javascript/controllers/breweries_controller.js
+++ app/javascript/controllers/breweries_controller.js
@@ -3,8 +3,7 @@ import {Controller} from "@hotwired/stimulus";
 export default class extends Controller {
     static targets = ["name", "year", "active"]
 
-    reset(event) {
-        event.preventDefault()
+    reset() {
         this.nameTarget.value = ""
         this.yearTarget.value = ""
         this.activeTarget.checked = false
